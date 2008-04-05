d2f35d3057afcb481cadf72be36f34a7ec396b80
Tom Aratyn <tom@securitycompass.com>
2008-04-05 01:09:03 +0000

    Removed a bunch of non code (labels, license) references to XSS Me. Related bug: bug 134

--- content/StreamListener.js
+++ content/StreamListener.js
@@ -3,20 +3,20 @@ Copyright 2007 Security Compass
 
 This file is part of SQL Inject Me.
 
-XSS Me is free software: you can redistribute it and/or modify
+SQL Inject Me is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-XSS Me is distributed in the hope that it will be useful,
+SQL Inject Me is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with XSS Me.  If not, see <http://www.gnu.org/licenses/>.
+along with SQL Inject Me.  If not, see <http://www.gnu.org/licenses/>.
 
-If you have any questions regarding XSS Me please contact
+If you have any questions regarding SQL Inject Me please contact
 tools@securitycompass.com
 */
 
--- content/TestManager.js
+++ content/TestManager.js
@@ -1,22 +1,22 @@
 /*
 Copyright 2008 Security Compass
 
-This file is part of XSS Me.
+This file is part of SQL Inject Me.
 
-XSS Me is free software: you can redistribute it and/or modify
+SQL Inject Me is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-XSS Me is distributed in the hope that it will be useful,
+SQL Inject Me is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with XSS Me.  If not, see <http://www.gnu.org/licenses/>.
+along with SQL Inject Me.  If not, see <http://www.gnu.org/licenses/>.
 
-If you have any questions regarding XSS Me please contact
+If you have any questions regarding SQL Inject Me please contact
 tools@securitycompass.com
 */
 
@@ -285,7 +285,7 @@ TestManager.prototype = {
         else {
             resultsManager = new ResultsManager(this.controller);
         }
-        resultsManager.showResults(this, "There was an error while testing this site. This was likely due to <a href='https://bugzilla.mozilla.org/show_bug.cgi?id=420025'>Mozilla bug 420025</a> which only affects Fx2. We're working on making XSS Me work with FireFox 3. Please help us track this bug by either <a href='mailto:bugs@securitycompass.com?subject=Triggered bug 420025'>emailing us</a> the url to this site or commenting on <a href='https://bugzilla.mozilla.org/show_bug.cgi?id=420025'>the bug</a>. We apologize for the inconvenience.");
+        resultsManager.showResults(this, "There was an error while testing this site. This was likely due to <a href='https://bugzilla.mozilla.org/show_bug.cgi?id=420025'>Mozilla bug 420025</a> which only affects Fx2. We're working on making SQL Inject Me work with FireFox 3. Please help us track this bug by either <a href='mailto:bugs@securitycompass.com?subject=Triggered bug 420025'>emailing us</a> the url to this site or commenting on <a href='https://bugzilla.mozilla.org/show_bug.cgi?id=420025'>the bug</a>. We apologize for the inconvenience.");
         this.controller.postTest();
         
         Components.utils.reportError(
